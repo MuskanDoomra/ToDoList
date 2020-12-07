7a8ad8a4db477dee56fd35e1e918fc0216606c5a
Muskan <69676535+MuskanDoomra@users.noreply.github.com>
2020-12-07 23:34:57 +0530

    Modified code
    Added checked functionality , scroll bar within list , some animations and deleteAll button

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+var deleteAll = document.getElementById("deleteAll");
+deleteAll.addEventListener('click',function() {
+    var child = list.lastElementChild;  
+        while (child) {
+            list.removeChild(child); 
+            child = list.lastElementChild; 
+        }
+})
+
 function handleClick() {
     var li = document.createElement("li");
     var inputValue = document.getElementById("input").value;
     var t = document.createTextNode(inputValue);
     li.appendChild(t);
     
-    if(inputValue == '') {
+    if(inputValue === '') {
         alert("Please write something");
     }
     else {
@@ -12,13 +28,34 @@ function handleClick() {
     }
     document.getElementById("input").value="";
 
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+    var cross = document.createElement("SPAN");
+    cross.innerHTML = '<i class="fa fa-trash-o" aria-hidden="true"></i>';
+    cross.className = "close";
+    li.appendChild(cross);
 
-    span.onclick = function(){
-        document.getElementById("myUL").removeChild(span.parentElement);
+    cross.onclick = function() {
+        removeItem(cross);
+        checkScroll();
+    }
+    checkScroll();
+}
+
+function removeItem(cross) {
+    cross.parentElement.className = "removed-item";
+    setTimeout(function(){document.getElementById("myUL").removeChild(cross.parentElement);}, 500);
+}
+
+function checkScroll() {
+    if(document.getElementById("ul_div").offsetHeight > 349) {
+        document.getElementById("ul_div").style.overflowY = "scroll";
+    }
+    else {
+        document.getElementById("ul_div").style.overflowY = "hidden";
+    }
+}
+
+function handleEvent(event) {
+    if(event.keyCode === 13) {
+        handleClick();
     }
 }
